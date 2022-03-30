916e3596abf9e560d93245f561ffc33573216223
Jake Inman <92830205+JakeInman@users.noreply.github.com>
2022-03-30 11:52:59 -0500

    altered error handling

--- app/main.py
+++ app/main.py
@@ -24,29 +24,46 @@ transactions = {
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Please enter your item."}
+    return {
+        "status": "Successful Login",
+        "message": "Hello, please enter your item."
+        }
 
 @app.get("/show-transaction/{transaction_id}")
 def read_item(transaction_id : int):
     if transaction_id not in transactions:
-        return {"Error": "Transaction does not exist"}
+        return {
+            "status": "ERROR: Item Not Found",
+            "message": "Sorry, we could not locate that transaction."
+            }
     return transactions[transaction_id]  
 
 @app.get("/make-change/{transaction_id}")
 def make_change(transaction_id : int):
+    if transaction_id not in transactions:
+        return {
+            "status": "ERROR: Item Not Found",
+            "message": "Sorry, we could not locate that transaction."
+            }
     return calculate_change(transactions[transaction_id].currency_inserted, transactions[transaction_id].price,  [25, 10, 5, 1])
 
 @app.post("/make-new-item-and-change/{transaction_id}")
 def make_change(transaction_id : int, transaction: Transaction):
     if transaction_id in transactions:
-        return {"Error": "Transaction already exists"}
+        return {
+            "status": "ERROR: Transaction Already Exists",
+            "messsage": "Sorry, that transaction already exists."
+            }
     transactions[transaction_id] = transaction
     return calculate_change(transactions[transaction_id].currency_inserted, transactions[transaction_id].price,  [25, 10, 5, 1])
 
 @app.put("/update-transaction/{transaction_id}")
 def update_transaction(transaction_id: int, transaction: UpdateTransaction):
     if transaction_id not in transactions:
-        return {"Error": "Transaction does not exist"}
+        return {
+            "status": "ERROR: Item Not Found",
+            "message": "Sorry, we could not locate that transaction."
+            }
 
     if transaction.item_name != None:
         transactions[transaction_id].item_name = transaction.item_name
@@ -62,6 +79,12 @@ def update_transaction(transaction_id: int, transaction: UpdateTransaction):
 @app.delete("/delete-transaction/{transaction_id}")
 def delete_transaction(transaction_id: int):
     if transaction_id not in transactions:
-        return {"Error": "Transaction does not exist"}
+        return {
+            "status": "ERROR: Item Not Found",
+            "message": "Sorry, we could not locate that transaction."
+            }
     del transactions[transaction_id]
-    return{"Message": "Transaction deleted successfully"}
+    return{
+        "status": "Successful Deletion",
+        "message": "Transaction deleted successfully."
+        }
