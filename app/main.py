3f9826c4ab8d0cbd07dc4a629c2c92c1ad059aa9
Jake Inman <92830205+JakeInman@users.noreply.github.com>
2022-04-24 19:26:37 -0500

    made features work

--- app/main.py
+++ app/main.py
@@ -14,24 +14,25 @@ class UpdateItem(BaseModel):
     price: Optional[int] = None
 
 class Cart(BaseModel):
-    item_list: dict
+    item_list: list
     item_count: int
     total_cost: int
     currency_inserted: int
 
-# class UpdateCart(BaseModel):
-#     item_list: Optional[dict] = None
-#     item_count: Optional[int] = None
-#     total_cost: Optional[int] = None
-#     currency_insterted: Optional[int] = None
-
+class UpdateCart(BaseModel):
+    item_list: Optional[list] = None
+    item_count: Optional[int] = None
+    total_cost: Optional[int] = None
+    currency_insterted: Optional[int] = None
 
 items = {
     1: Item(name="apple", price=125),
     2: Item(name="orange", price=200)
 }
 
-cart = [] 
+carts = {
+    1: Cart(item_list = [{"name": "apple", "price": 125}, {"name": "apple", "price": 125}, {"name": "orange", "price": 200}], item_count=3, total_cost=450, currency_inserted=600)
+}
 
 @app.get("/")
 def read_root():
@@ -50,22 +51,22 @@ def read_item(item_id : int):
     return items[item_id]
 
 @app.get("/view-cart")
-def view_cart(cart):
-    if cart.length == 0:
+def view_cart(cart_id: int):
+    if carts[cart_id].item_count == 0:
         return {
             "status": "ERROR: Cart Is Empty",
             "message": "Sorry, we could not locate any items in your cart."
             }
-    return cart
+    return carts[cart_id]
 
 @app.get("/checkout")
-def checkout(cart):
-    if len(cart) == 0:
+def checkout(cart_id: int):
+    if carts[cart_id] == 0:
         return {
             "status": "ERROR: Cart Is Empty",
             "message": "Sorry, we could not locate any items in your cart."
             }
-    return calculate_change(cart[currency_inserted], cart[total_cost],  [25, 10, 5, 1])
+    return calculate_change(carts[cart_id].currency_inserted, carts[cart_id].total_cost,  [25, 10, 5, 1])
 
 @app.post("/make-new-item/{item_id}")
 def make_new_item(item_id : int, item: Item):
@@ -78,14 +79,14 @@ def make_new_item(item_id : int, item: Item):
     return item
 
 @app.post("/add-item-to-cart/{item_id}")
-def add_item_to_cart(item_id: int):
+def add_item_to_cart(item_id: int, cart_id: int):
     if item_id not in items:
         return {
             "status": "ERROR: Item Not Found",
             "message": "Sorry, we could not locate that item."
             }
-    cart.append(items[item_id])
-    return cart
+    carts[cart_id].item_list.append(items[item_id])
+    return carts[cart_id]
 
 
 @app.put("/update-item/{item_id}")
@@ -101,9 +102,6 @@ def update_item(item_id: int, item: UpdateItem):
 
     if item.price != None:
         items[item_id].price = item.price
-
-    if item.currency_insterted != None:
-        items[item_id].currency_inserted = item.currency_insterted
 
     return items[item_id]
 
